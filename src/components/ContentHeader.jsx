d667a4ec78085ec52a136a612e196d45ef003034
ManuelPrhyme <82534882+ManuelPrhyme@users.noreply.github.com>
2025-04-12 22:22:19 +0300

    Delete src directory

--- src/components/ContentHeader.jsx
+++
@@ -1,9 +0,0 @@
-const ContentHeader = ({ title = "Home" }) => {
-  return (
-    <div className="content--header">
-      <h1 className="header--title">{title}</h1>
-    </div>
-  )
-}
-
-export default ContentHeader
